bf8b4601bdbec04786f7c9f435dc414d66a5ebe3
Jaime Cabello Venegas <jcabellov@CLRTLC02FK6AFML7H.local>
2023-03-01 00:30:56 -0300

    add libraries for project

--- index.js
+++ index.js
@@ -3,7 +3,28 @@
  */
 
 import {AppRegistry} from 'react-native';
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
